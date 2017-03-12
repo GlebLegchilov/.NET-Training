8f3c25d37e59de8e0d22d976fc0d086b693c4de1
GlebLegchilov <betrazen13@gmail.com>
2017-03-12 21:13:02 +0300

    Add files via upload

---
+++ NET.S.2017.01.Legchilov.02/ConsoleMergeSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MergeSort;
+
+namespace ConsoleMergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32[] array = new Int32[100];
+            Random rd = new Random();
+            for (int i = 0; i < array.Length; ++i)
+                array[i] = rd.Next(1, 1001);
+            
+            for(int i = 0; i < array.Length; ++i)
+                System.Console.Write(array[i] + " ");
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+
+            array = MergeSort.SortClass.Sort(array);
+
+            for (int i = 0; i < array.Length; ++i)
+                System.Console.Write(array[i] + " ");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ NET.S.2017.01.Legchilov.02/MergeSort/SortClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace MergeSort
+{
+
+    /// <summary>
+    /// Class sort unsorted array
+    /// </summary>
+    static public class SortClass
+    {
+
+        /// <summary>
+        /// Sorts an array by MergeSort
+        /// </summary>
+        /// <param name="array">Unsorted array</param>
+        /// <returns>Sorted array</returns>
+        static public Int32[] Sort(Int32[] array)
+        {
+            if (array.Length == 1)
+                return array;
+            int mid_point = array.Length / 2;
+            return Merge(Sort(array.Take(mid_point).ToArray()), Sort(array.Skip(mid_point).ToArray()));
+        }
+
+        /// <summary>
+        /// Merge two arrays
+        /// </summary>
+        /// <param name="firstArray">First array's half</param>
+        /// <param name="secondArray">Second array's half</param>
+        /// <returns>Merged array</returns>
+        static private Int32[] Merge(Int32[] firstArray, Int32[] secondArray)
+        {
+            Int32 a = 0, b = 0;
+            Int32[] mergedArray = new Int32[firstArray.Length + secondArray.Length];
+            for (int i = 0; i < firstArray.Length + secondArray.Length; i++)
+            {
+                if (b.CompareTo(secondArray.Length) < 0 && a.CompareTo(firstArray.Length) < 0)
+                    if (firstArray[a].CompareTo(secondArray[b]) > 0)
+                        mergedArray[i] = secondArray[b++];
+                    else
+                        mergedArray[i] = firstArray[a++];
+                else
+                    if (b < secondArray.Length)
+                    mergedArray[i] = secondArray[b++];
+                else
+                    mergedArray[i] = firstArray[a++];
+            }
+            return mergedArray;
+        }
+    }
+}
